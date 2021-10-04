9251aeff9917abb39880b54f6810bdddf82e1a3d
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-10-04 23:02:28 +0900

    db test
    ec2 db test

--- asd.py
+++ asd.py
@@ -9,9 +9,8 @@ import json
 mydb = pymysql.connect(
         user='root',
         passwd='root',
-        host='127.0.0.1',
+        host='localhost',
         db='sulivan',
-        port=3306,
         charset='utf8'
     )
 
