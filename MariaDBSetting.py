00622924c5aeae97ae321d36456769d1cf7a1018
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-10-09 17:12:27 +0900

    DB Setting
    DB Setting test

--- MariaDBSetting.py
+++ MariaDBSetting.py
@@ -1,3 +1,5 @@
+import csv
+
 import requests
 from bs4 import BeautifulSoup
 import pymysql
@@ -16,21 +18,16 @@ mydb = pymysql.connect(
 
 cur = mydb.cursor()
 cur.execute("truncate event;")
-"""cur.execute("truncate nutritionfacts")"""
+cur.execute("truncate nutritionfacts")
 
-"""
-df1 = pd.read_csv("한국소비자원 생필품 및 서비스 가격 정보_20210827.csv", encoding='euc-kr', index_col=0)
-df1.drop(['Unnamed: 1'], axis = 1, inplace=True)
-print(df1.loc[df1['판매업소'] == 'CU(본사)'])
-print(df1.loc[df1['판매업소'] == 'GS25(본사)'])
-print(df1.loc[df1['판매업소'] == '세븐일레븐(본사)'])
-"""
+
 
 url = 'https://pyony.com/search/?event_type=&category=1&item=&sort=&q='
 url2 = "https://www.fatsecret.kr/%EC%B9%BC%EB%A1%9C%EB%A6%AC-%EC%98%81%EC%96%91%EC%86%8C/search?q=%ec%9d%8c%eb%a3%8c%ec%88%98&pg="
 insert_query = "insert into event (shopname, beveragename, eventname, price) values (%s, %s, %s, %s);"
 insert_query2 = "insert into nutritionfacts (foodname, kcal, carbohydrate, protein, Fat) values (%s, %s, %s, %s, %s);"
-for page in range(0, 36):
+insert_query3 = "insert into beverage (beveragename, price, convenience) values (%s, %s, %s);"
+for page in range(0, 0):
     response = requests.get(url2+str(page))
     for i in range(0, 10):
         if response.status_code == 200:
@@ -47,7 +44,7 @@ for page in range(0, 36):
             Fat = soup2.select('.nutrient.black.right.tRight')[3].get_text()
             cur.execute(insert_query2, (foodname, kcal, carbohydrate, protein, Fat))
             mydb.commit()
-for page in range(0, 80):
+for page in range(0, 0):
     response = requests.get(url+str(page))
 
     if response.status_code == 200:
@@ -74,4 +71,27 @@ for page in range(0, 80):
     else:
         print(response.status_code)
 
+df1 = pd.read_csv("한국소비자원 생필품 및 서비스 가격 정보_20210827.csv", encoding='euc-kr', index_col=0)
+df1.drop(['Unnamed: 1'], axis = 1, inplace=True)
+cu = df1.loc[df1['판매업소'] == 'CU(본사)']
+gs25 = df1.loc[df1['판매업소'] == 'GS25(본사)']
+seveneleven = df1.loc[df1['판매업소'] == '세븐일레븐(본사)']
+for index, row in cu.iterrows():
+    price = row[0]
+    convenience = row[1]
+    beveragename = index
+    cur.execute(insert_query3, (beveragename, price, convenience))
+    mydb.commit()
+for index, row in gs25.iterrows():
+    price = row[0]
+    convenience = row[1]
+    beveragename = index
+    cur.execute(insert_query3, (beveragename, price, convenience))
+    mydb.commit()
+for index, row in seveneleven.iterrows():
+    price = row[0]
+    convenience = row[1]
+    beveragename = index
+    cur.execute(insert_query3, (beveragename, price, convenience))
+    mydb.commit()
 print("Done")
