9b7897f53a1f8fd5438f7ff46f1a1c5c11e4c878
taeminHan <taemin9705@gmail.com>
2021-10-25 22:18:14 +0900

    모델 추가학습 적용

--- Detection_img.py
+++ Detection_img.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
+
 if gpus:
     try:
         # Currently, memory growth needs to be the same across GPUs
@@ -13,6 +14,7 @@ if gpus:
     except RuntimeError as e:
         # Memory growth must be set before GPUs have been initialized
         print(e)
+
 
 def predict(f):
     image_w = 64
@@ -31,22 +33,29 @@ def predict(f):
     model = load_model('capstone_model.h5')
 
     prediction = model.predict(X)
+
+    print(prediction)
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
     cnt = 0
 
     for i in prediction:
+        # ["코카콜라", "레드불", "밀키스", "칠성사이다", "환타"]
         pre_ans = i.argmax()  # 예측 레이블
         pre_ans_str = ''
         if pre_ans == 0:
             pre_ans_str = "코카콜라"
         elif pre_ans == 1:
-            pre_ans_str = "칠성사이다"
+            pre_ans_str = "레드불"
         elif pre_ans == 2:
-            pre_ans_str = "스프라이트"
+            pre_ans_str = "밀키스"
+        elif pre_ans == 3:
+            pre_ans_str = "칠성사이다"
 
         if i[0] >= 0.8:
             return pre_ans_str
         if i[1] >= 0.8:
             return pre_ans_str
         if i[2] >= 0.8:
+            return pre_ans_str
+        if i[3] >= 0.8:
             return pre_ans_str
